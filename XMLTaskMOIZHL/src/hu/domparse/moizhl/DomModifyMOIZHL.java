df82290b3666d843b76f142c0ae77e0746f6dc16
zsirosnikolett <zsirosnikolett971121@gmail.com>
2021-12-06 00:53:22 +0100

    corrected some mistakes

--- XMLTaskMOIZHL/src/hu/domparse/moizhl/DomModifyMOIZHL.java
+++ XMLTaskMOIZHL/src/hu/domparse/moizhl/DomModifyMOIZHL.java
@@ -1,54 +1,193 @@
 package hu.domparse.moizhl;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
+import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-
+import java.io.IOException;
+import java.util.Scanner;
 
 public class DomModifyMOIZHL {
-	 public static void main(String[] args) {
-    NodeList nodeList;
-    try{
-        DocumentBuilderFactory factory =DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder=factory.newDocumentBuilder();
-        //Fájl beolvasása
-        Document document=builder.parse(new File("XMLmoizhl.xml"));
-        document.getDocumentElement().normalize();
-        //Aktuális elem meghatározása
-        nodeList=document.getElementsByTagName("");
-        for(int i =0;i<nodeList.getLength();i++){
-            Node node=nodeList.item(i);
-            System.out.println("\nAktuális elem: " + node.getNodeName());
-           
-            if(node.getNodeType()==Node.ELEMENT_NODE){
-                Element element=(Element) node;
-
-                            
-            }
-        
-    }
-
-                Transformer transformer= TransformerFactory.newInstance().newTransformer();
-                Source input=new DOMSource(document);
-                Result output=new StreamResult(new File("XMLMOIZHLModify.xml"));
-                transformer.transform(input,output);
-                
-            }catch (ParserConfigurationException e){
-                e.printStackTrace();
-            } catch (Exception e){
-                e.printStackTrace();
-            
-            }
-}
-}
+	 public static void main(String[] args) 
+		 throws ParserConfigurationException, IOException, SAXException, TransformerException {
+				File xmlFile = new File("XMLmoizhl.xml"); // xml fájl bekérés
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); // olvasás lehetové tétele
+				DocumentBuilder dBuilder = factory.newDocumentBuilder();
+				Document doc = dBuilder.parse(xmlFile);
+				doc.getDocumentElement().normalize();
+				System.out.println("XML Módosítása");
+				System.out.println("Adja meg mit szeretne módosítani: ");
+				System.out.println("1 Beszállító módosítása\n2 Pizza módosítása\n3 Futár módosítása");
+				Modify(doc);
+			}
+		 
+		 
+	 public static void ModifyXML(Document doc) throws TransformerException {
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("XMLMOIZHL.xml"));
+			transformer.transform(source, result);
+		}
+	 
+	 
+	 public static void Modify(Document doc) throws TransformerException {
+			int BeszallitokSzama = doc.getElementsByTagName("Beszallito").getLength(); // beszallitok számának lekérdezése
+			int PizzakSzama = doc.getElementsByTagName("Pizza").getLength(); // pizzak számának lekérdezése
+			int FutarokSzama = doc.getElementsByTagName("Futar").getLength(); // futarok számának lekérdezése
+			
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Adja meg a sorszamot: ");
+			int readCategory = scan.nextInt();
+			switch (readCategory) {
+			case 1:
+				ModifyBeszallitok(doc, BeszallitokSzama);
+				break;
+			case 2:
+				ModifyPizzak(doc, PizzakSzama);
+				break;
+			case 3:
+				ModifyFutarok(doc, FutarokSzama);
+				break;
+		
+			}
+			scan.close();
+		}
+	 
+	 public static String ReadId() {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("\nid:");
+			String id = sc.nextLine();
+			return id;
+		}
+	 private static void ModifyBeszallitok(Document doc, int beszallitoszam) throws TransformerException {
+			// Kiiratjuk a jelenlegi Beszállítókat, majd lekérdezzük melyiket kívánja
+			// módosítani.
+			System.out.println("Melyik Beszállítónak az adatait szeretné módosítani?");
+			for (int i = 1; i < beszallitoszam + 1; i++) {
+				System.out.println(i + ". beszállító");
+				DomReadMOIZHL.ReadBeszallitoById(doc, String.valueOf(i));
+				System.out.println("----------------------------------------------");
+			}
+			String id = ReadId();
+			// Bekérjük az új adatokat
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Név: ");
+			String Nev = sc.nextLine();
+			System.out.print("Iranyitoszam: ");
+			String Iranyitoszam = sc.nextLine();
+			System.out.print("Varos: ");
+			String Varos = sc.nextLine();
+			System.out.print("Utca: ");
+			String Utca = sc.nextLine();
+			System.out.print("Hazszam: ");
+			String Hazszam = sc.nextLine();
+			sc.close();
+			// lekérdezzük az Elementeket, majd setTextContent-el módosítjuk.
+			NodeList nodeList = doc.getElementsByTagName("Beszallito");
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node nNode = nodeList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) nNode;
+					String sid = element.getAttribute("id");
+					if (sid.equals(id)) {
+						Node node1 = element.getElementsByTagName("Nev").item(0);
+						node1.setTextContent(Nev);
+						Node node2 = element.getElementsByTagName("Iranyitoszam").item(0);
+						node2.setTextContent(Iranyitoszam);
+						Node node3 = element.getElementsByTagName("Varos").item(0);
+						node3.setTextContent(Varos);
+						Node node4 = element.getElementsByTagName("Utca").item(0);
+						node4.setTextContent(Utca);
+						Node node5 = element.getElementsByTagName("Hazszam").item(0);
+						node5.setTextContent(Hazszam);
+						System.out.println("Sikeres módosítás");
+					}
+				}
+			}
+			ModifyXML(doc); // Létrehozzuk az XML-t
+	 }
+	 private static void ModifyPizzak(Document doc, int pizzaszam) throws TransformerException {
+			System.out.println("Melyik pizzát kívánja módosítani?");
+			for (int i = 1; i < pizzaszam + 1; i++) {
+				System.out.println(i + ". pizza");
+				DomReadMOIZHL.ReadPizzaById(doc, String.valueOf(i));
+				System.out.println("----------------------------------------------");
+			}
+			String id = ReadId();
+			// Bekérjük az új adatokat
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Meret: ");
+			String Meret = sc.nextLine();
+			System.out.print("Teljes_ar: ");
+			String Teljes_ar = sc.nextLine();
+			System.out.print("Pizza_neve: ");
+			String Pizza_neve = sc.nextLine();
+			sc.close();
+			// lekérdezzük az Elementeket, majd setTextContent-el módosítjuk.
+			NodeList nodeList = doc.getElementsByTagName("Pizza");
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node nNode = nodeList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) nNode;
+					String sid = element.getAttribute("id");
+					if (sid.equals(id)) {
+						Node node1 = element.getElementsByTagName("Meret").item(0);
+						node1.setTextContent(Meret);
+						Node node2 = element.getElementsByTagName("Teljes_ar").item(0);
+						node2.setTextContent(Teljes_ar);
+						Node node3 = element.getElementsByTagName("Pizza_neve").item(0);
+						node3.setTextContent(Pizza_neve);
+						System.out.println("Sikeres módosítás");
+					}
+				}
+			}
+			ModifyXML(doc); // Létrehozzuk az XML-t
+		}
+	 private static void ModifyFutarok(Document doc, int futarszam) throws TransformerException {
+			System.out.println("Melyik Futárt kívánja módosítani?");
+			for (int i = 1; i < futarszam + 1; i++) {
+				System.out.println(i + ". pizza");
+				DomReadMOIZHL.ReadFutarById(doc, String.valueOf(i));
+				System.out.println("----------------------------------------------");
+			}
+			String id = ReadId();
+			// Bekérjük az új adatokat
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Nev: ");
+			String Nev = sc.nextLine();
+			System.out.print("Telefonszam: ");
+			String Telefonszam = sc.nextLine();
+			sc.close();
+			// lekérdezzük az Elementeket, majd setTextContent-el módosítjuk.
+			NodeList nodeList = doc.getElementsByTagName("Futar");
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node nNode = nodeList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) nNode;
+					String sid = element.getAttribute("id");
+					if (sid.equals(id)) {
+						Node node1 = element.getElementsByTagName("Nev").item(0);
+						node1.setTextContent(Nev);
+						Node node2 = element.getElementsByTagName("Telefonszam").item(0);
+						node2.setTextContent(Telefonszam);
+						System.out.println("Sikeres módosítás");
+					}
+				}
+			}
+			ModifyXML(doc); // Létrehozzuk az XML-t
+		}
+	 
+	 
+	 }
+	 
--- XMLTaskMOIZHL/src/hu/domparse/moizhl/DomQueryMOIZHL.java
+++ XMLTaskMOIZHL/src/hu/domparse/moizhl/DomQueryMOIZHL.java
@@ -1,53 +1,93 @@
 package hu.domparse.moizhl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.IOException;
 
 public class DomQueryMOIZHL {
 	
+	public static void main(String[] args)
+			throws ParserConfigurationException, IOException, SAXException, TransformerException {
+		
+		File xmlFile = new File("XMLmoizhl.xml"); // xml fájl bekérése
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); // olvasás lehetové tétele
+		DocumentBuilder dBuilder = factory.newDocumentBuilder();
+		Document doc = dBuilder.parse(xmlFile);
+		doc.getDocumentElement().normalize();
 
-	 public static void main(String[] args) {
-	        try {
-	            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	            DocumentBuilder builder = factory.newDocumentBuilder();
-	            // fájl beolvasása
-	            Document document = builder.parse(new File("XMLmoizhl.xml"));
-	            document.getDocumentElement().normalize();
-	            //Xpath készítése
-	            XPath xPath= XPathFactory.newInstance().newXPath();
-	            
-	           // String expression=;
-	            
-	            NodeList nodeList=(NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
-	            
-	            for (int i=0;i<nodeList.getLength();i++){
-	                Node node=nodeList.item(i);
-	                System.out.println ("\nAktuális elem: " + node.getNodeName());
-	                
-	                if(node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("")){
-	                    Element element=(Element) node;
-	                    System.out.println("" + element.getAttribute(""));
-	                    System.out.println("" + element.getAttribute(""));
-	                    System.out.println(" " + element.getAttribute(""));
-	                    System.out.println(""
-	                            + element.getElementsByTagName("").item(0).getTextContent());
-
-	                }
-	            }
-	        } catch (ParserConfigurationException | XPathExpressionException | IOException | SAXException e){
-	            e.printStackTrace();
-	        }
-	    }
+		System.out.println("Root elem: " + doc.getDocumentElement().getNodeName());
+		System.out.println("--------------------------------------");
+		LoadPizzazoQuery(doc);
+	
 	}
+	//Kilistázza az összes Pizzázó nevét
+	public static void LoadPizzazoQuery(Document doc) throws TransformerException {
+		NodeList nodeList = doc.getElementsByTagName("Pizzazo"); // Pizzázó elemek listázása
+		String Pizzazo;
+		Element element = null;
+		Node nNode = null;
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			nNode = nodeList.item(i);
+			element = (Element) nNode;
+			String Nev = element.getElementsByTagName("Nev").item(0).getTextContent();
+			System.out.println((i + 1) + ") " + Nev);
+
+	
+		}
+	
+		
+		System.out.println("Írja be annak a Pizzázónak a nevét, amelyikbõl látni szeretné a Beszállítói adatait:");
+		Scanner sc = new Scanner(System.in);
+		Pizzazo = sc.nextLine();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			nNode = nodeList.item(i);
+			element = (Element) nNode;
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				if (Pizzazo.equals("Pizza Hut")) {
+					LoadBeszallitoQuery(doc, "01");
+					break;
+				}
+
+				if (Pizzazo.equals("Pizza Bazis")) {
+					LoadBeszallitoQuery(doc, "02");
+					break;
+				}
+			}
+		}
+		sc.close();
+	}
+		//A kiválasztott Pizzázó Beszallitoinak kiírása
+		public static void LoadBeszallitoQuery(Document doc, String id) throws TransformerException {
+			NodeList nodeList = doc.getElementsByTagName("Beszallito");
+			int Beszallito = 0;
+
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node nNode = nodeList.item(i);
+				Element element = (Element) nNode;
+				String PizzazoID = element.getAttribute("BeszallitoID");
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					if (id.equals(PizzazoID)) {
+						Beszallito+= 1;
+						System.out.println(Beszallito + ". Beszallitók adatai:");
+						String BeszallitoID = element.getAttribute("BeszallitoID");
+						DomReadMOIZHL.ReadBeszallitoById(doc, BeszallitoID);
+
+					}
+	}
+			}
+		}
+}
